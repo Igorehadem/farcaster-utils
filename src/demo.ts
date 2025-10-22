5063292b51099a20b9a1061ee72f5e1edac4febc
Igorehadem <140109468+Igorehadem@users.noreply.github.com>
2025-10-22 08:37:11 +0300

    feat: add demo script

---
+++ src/demo.ts
@@ -0,0 +1,3 @@
+import { nowISO, truncate } from "./utils.js";
+console.log("Farcaster Utils Demo @", nowISO());
+console.log(truncate("This is a demo output to keep the repo alive and useful.", 40));
